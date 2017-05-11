cd250bf54ff2363d90f08ad21e00c98458e40cb9
huwenjing <huwenjing215@gmail.com>
2017-05-11 10:02:08 +0800

    only admin can acess backend panel

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ApplicationRecord
+
+  has_many :cart_items
+  has_many :products, through: :cart_items, source: :product
   def add_product_to_cart
     ci = cart_items.build
     ci.product = product
